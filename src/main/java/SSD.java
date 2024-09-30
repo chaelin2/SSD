af2080a1ebda27ebdc2567934ad5bd6ed630ac20
LYJ22 <pippi478@gmail.com>
2024-09-30 12:26:26 +0900

    ssd write command

--- src/main/java/SSD.java
+++ src/main/java/SSD.java
@@ -1,13 +1,31 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class SSD {
+    static File fileWrite = new File("result.txt");
+
+    // write면 lineNum = 1, fullwrite면 lineNum = 100
+    static void writeResult(String line, int lineNum){
+        try{
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileWrite));
+            if(fileWrite.canWrite()){
+                for(int i = 0; i < lineNum; i++) {
+                    bufferedWriter.write(line);
+                    bufferedWriter.newLine();
+                }
+                bufferedWriter.close();
+            }
+        }catch (IOException e){
+            System.out.println("cannot write result.txt");
+        }
+    }
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("nand.txt"));
         while(true) {
             String line = br.readLine();
             if(line==null) break;
+            writeResult(line, 100);
             System.out.println(line);
         }
         br.close();
