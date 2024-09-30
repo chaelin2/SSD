69ec6db0bea1193f2133380ab10b84f061784484
yunju Lee <76641389+shining013@users.noreply.github.com>
2024-09-30 16:52:27 +0900

    Update SSD.java

--- src/main/java/SSD.java
+++ src/main/java/SSD.java
@@ -65,9 +65,17 @@ public class SSD {
 
     public static void main(String[] args) throws IOException {
         SSD sd = new SSD();
-        int index = Integer.parseInt(args[1]);
+        int index = 0;
+        if(args.length>=2){
+            index = Integer.parseInt(args[1]);
+        }
         if(args.length == 3) {
             sd.writeValue(index, args[2]);
+        } else if (args[0].equals("fullread")) {
+            sd.initResult();
+            for(int i = 0; i < 100; i++){
+                sd.readNand(i);
+            }
         } else {
             sd.initResult();
             sd.readNand(index);
